897169cccec30508c4129404d03e8c6d643a6c08
Max Shvagerus <mshvagerus@gmail.com>
2023-01-23 05:56:08 +0300

    tests for junit4-platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,11 @@
 package ru.netology.service;
 
-import org.junit.Test;
+//import org.junit.Test;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService cashback = new CashbackHackService();
@@ -35,5 +38,35 @@ public class CashbackHackServiceTest {
         int actual = cashback.remain(amount);
 
         assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void shouldAmountEqual1000Junit5() {
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = cashback.remain(amount);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAmountAbove1000Junit5() {
+        int amount = 1200;
+
+        int expected = 800;
+        int actual = cashback.remain(amount);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAmountBelow1000Junit5() {
+        int amount = 800;
+
+        int expected = 200;
+        int actual = cashback.remain(amount);
+
+        Assert.assertEquals(expected, actual);
     }
 }
